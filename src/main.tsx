151d4d3907c3f1ade64a8888b6e50ccb09fc9983
zagzy8776 <127265311+zagzy8776@users.noreply.github.com>
2026-09-17 20:48:39 +0200

    wire AdminAuthProvider for strict admin-only session

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
+import { AdminAuthProvider } from './context/AdminAuthContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <AdminAuthProvider>
+          <App />
+        </AdminAuthProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
